4487de47d0ccda019c32e149901fbe6dd8d63dbb
Luan Ferreira <luanhsouzaf@gmail.com>
2023-10-26 14:14:57 -0300

    Projeto criado

---
+++ src/main/java/com/example/lfbank/LfBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.lfbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LfBankApplication.class, args);
+    }
+
+}
